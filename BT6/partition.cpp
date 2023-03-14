baef7c8ec6268ac7456dc0f0142d4c8fd2343583
Tuanas125 <113823199+Tuanas125@users.noreply.github.com>
2023-03-14 22:23:21 +0700

    Add files via upload

--- BT6/partition.cpp
+++ BT6/partition.cpp
@@ -1,6 +1,26 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int main(){
 
+void doitien(ll n, vector<ll> a){
+    if(n==0){
+        for(ll i=0; i<a.size(); i++)
+            cout << a[i] << " ";
+        cout << "\n";
+        return;
+    }
+    for(ll i=1; i<=n; i++){
+        if(a.empty()||i>=a.back()){
+            a.push_back(i);
+            doitien(n-i, a);
+            a.pop_back();
+        }
+    }
+}
+
+int main(){
+    ll n;
+    cin >> n;
+    vector<ll> a;
+    doitien(n,a);
 }
